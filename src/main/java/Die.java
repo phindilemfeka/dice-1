0feacd22e4b116dfb2ebbef97a87bbe142a40a03
Recruit <you@example.com>
2020-02-24 11:32:52 +0200

    update

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -11,25 +11,25 @@ public class Die {
 
     public void roll() throws Exception {
         dieArray = new int[sides]; //die array to hold the sides of the die
-        for (int i = 0; i <dieArray.length; i++) {
-            dieArray[i] = i+1;
+        for (int i = 0; i < dieArray.length; i++) {
+            dieArray[i] = i + 1;
         }
         int sum = getSum();
-        int randomise = (int) (Math.random()*sum+1); //randomising the sum of probability
-        for (int i = 0; i <sides; i++) {
+        int randomise = (int) (Math.random() * sum + 1); //randomising the sum of probability
+        for (int i = 0; i < sides; i++) {
             randomise -= this.probabilities[i]; //random value - weight of the die
-            if(randomise <=0){ //if random value is less/equal to 0...
-               value = dieArray[i]; //return the value of that die
-               return;
+            if (randomise <= 0) { //if random value is less/equal to 0...
+                value = dieArray[i]; //return the value of that die
+                return;
             }
-            if(this.probabilities[i]<0){
+            if (this.probabilities[i] < 0) {
                 throw new Exception("negative probabilities not allowed");
             }
-            if(sum<1){
+            if (sum < 1) {
                 throw new Exception("probability sum must be greater than 0");
             }
         }
-    }
+}
      /*Calculating the sum of probabilities or the weight of the die*/
     private int getSum() {
         int sum=0;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Die die6 = new Die(6,1,100,1,1,100,2);
+        Die die6 = new Die(6,1,1,1,1,1,2);
         die6.roll();
 
-        Die die20 = new Die(20);
-        die20.roll();
+      /*  Die die20 = new Die(20);
+        die20.roll();*/
 
         System.out.print(die6.value); // this would print a number between 1 and 6 inclusive
 
-        System.out.print(die20.value); // this would print a number between 1 and 20 inclusive
+        //System.out.print(die20.value); // this would print a number between 1 and 20 inclusive
     }
 }
